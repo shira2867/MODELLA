dda840d7d4bd54540cb22eaf75b9937d589fe17b
hadasHadasShira <hadas31949@gmail.com>
2025-11-11 10:05:29 +0200

    add faq

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,13 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "res.cloudinary.com", // אם את משתמשת גם ב-Cloudinary
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
-        hostname: "lh3.googleusercontent.com", // זה מוסיף תמונות מה-Google Profile
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
       },
     ],
   },
