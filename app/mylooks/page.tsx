d10660c687399844ae0afa55f5738b286f8d0a26
hadasHadasShira <hadas31949@gmail.com>
2025-11-16 14:32:29 +0200

    add myLooks page

--- app/mylooks/page.tsx
+++ app/mylooks/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 import MyLooks from "@/app/Components/MyLooks/MyLooks";
 export default function ShowMyLooks() {
-  const userId = "123"; // כאן תמשכי את ה-userId מהסשן או פרופ
+  const userId = "user_12345"; // כאן תמשכי את ה-userId מהסשן או פרופ
 
   return (
     <div style={{ padding: "2rem" }}>
