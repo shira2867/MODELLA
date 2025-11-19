d94ab3d74d0ab703536c23f479363176a55aa0f0
RachelAdi <r0548571737@gmail.com>
2025-11-19 13:52:18 +0200

    continue to fix the styles

--- app/mycloset/page.tsx
+++ app/mycloset/page.tsx
@@ -2,13 +2,11 @@
 import MyCloset from "@/app/Components/MyCloset/MyCloset";
 import NewLook from "@/app/Components/NewLook/NewLook";
 import Footer from "../Components/Footer/Footer";
-import BurgerMenu from '../Components/BurgerMenu/BurgerMenu';
 import { useEffect, useState } from "react";
 import styles from "./mycloset.module.css";
 import Header from "../Components/Header/Header";
 
 export default function ShowMyCloset() {
-  
   const [userId, setUserId] = useState<string | null>(null);
   useEffect(() => {
     const storedUserId = localStorage.getItem("userId");
@@ -18,8 +16,11 @@ export default function ShowMyCloset() {
     return <div>Loading...</div>;
   }
   return (
-   <div className={styles.pageContainer}>
-    <Header />
+    <div className={styles.pageContainer}>
+      <Header />
+      <div className={styles.pageTitleWrapper}>
+        <h1 className={styles.title}>My Closet</h1>
+      </div>
       <div className={styles.mainArea}>
         <NewLook />
         <MyCloset userId={userId} />
