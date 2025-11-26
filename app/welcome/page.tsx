97bd8e9099421a5fa383ca0b15270f892bff1d6c
hadasHadasShira <hadas31949@gmail.com>
2025-11-26 11:49:35 +0200

    fix vercel error

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,54 +1,12 @@
-"use client";
-import Image from "next/image";
+// app/welcome/page.tsx
 
-import styles from "./Welcome.module.css";
+import { Suspense } from "react";
+import Welcome from "../Components/Welcome/Welcome";
 
-import logo from "../../public/logo.png";
-import { useRouter, useSearchParams } from "next/navigation";
-
-export default function Welcome() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const redirectLookId = searchParams.get("redirectLookId");
-
-  const handleRedirect = (path: string) => {
-    if (redirectLookId) {
-      localStorage.setItem("redirectLookId", redirectLookId);
-      console.log("Saved redirectLookId:", redirectLookId);
-    }
-    router.replace(path);
-  };
-
+export default function WelcomePage() {
   return (
-    <div className={styles.welcome}>
-      <div className={styles.Container}>
-        <Image
-          className={styles.logo}
-          src={logo}
-          alt="Project Logo"
-          width={400}
-          height={200}
-        />
-        <h1 className={styles.title}>Dress. Style. Shine.</h1>
-        <p className={styles.subtitle}>
-          Discover your perfect look with SnapFit{" "}
-        </p>
-        <p className={styles.subtitle}>- where fashion meets technology!</p>
-        <div className={styles.buttonContainer}>
-          <button
-            className={styles.button}
-            onClick={() => handleRedirect("/register")}
-          >
-            Join Us
-          </button>
-          <button
-            className={styles.button}
-            onClick={() => handleRedirect("/login")}
-          >
-            Member Login
-          </button>
-        </div>
-      </div>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Welcome />
+    </Suspense>
   );
 }
